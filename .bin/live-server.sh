bb5e00229a51f35b78067c088dfe787548dfa984
WWWCourses <ivawebcourses@gmail.com>
2019-02-10 01:00:26 +0200

    dotfiles tracked

---
+++ .bin/live-server.sh
@@ -0,0 +1,5 @@
+live-server --port=2222 \
+--open=/ProgressBG-JS-Advanced \
+--watch=/ProgressBG-JS-Advanced  /home/nemsys/projects/courses/ProgressBG &>/dev/null &
+
+echo 'live-server started on port=2222 for ProgressBG-JS-Advanced'
